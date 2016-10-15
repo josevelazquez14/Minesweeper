f1408b07a888e57424fee25e9b0af690da97f54f
Ademir <Ademir@DESKTOP-NFA2GKI>
2016-10-14 23:30:13 -0400

    project 1: Minesweeper

--- src/MineBoard.java
+++ src/MineBoard.java
@@ -14,7 +14,7 @@ public class MineBoard {
 		// Dummy Positioning
 		String[][] position = new String[10][10];
 		
-		while(numberMines < 10) //create the mines
+		while(numberMines < 10)  //create the mines
 		{
 			// Create Mine
 			Mines[numberMines] = new Mine(generator.nextInt(9), generator.nextInt(9));
@@ -22,13 +22,18 @@ public class MineBoard {
 			// Cols and rows
 			int col = generator.nextInt(9);
 			int row = generator.nextInt(9);
-			
+		
+			while(generator.nextInt() != generator.nextInt())
+			{
 			// Assign bomb
 			position[col][row]="bomb";
 			
 			// Check if item already has a bomb
 			if (Mines[numberMines].Col  > 0 && Mines[numberMines].Row > 0 && position[Mines[numberMines].Col][Mines[numberMines].Row] != "bomb")
 				numberMines++;
-		}
+				
 	}
+			
+}
+	
 }
